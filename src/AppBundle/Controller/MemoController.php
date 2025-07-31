040ef9cf0ca00c6e9345c57bf584cd6c7d030e5b
Muragaruhae <jac8for@gmail.com>
2025-07-31 18:42:43 +0900

    メモ編集機能を追加し、編集および削除のルートを実装

--- src/AppBundle/Controller/MemoController.php
+++ src/AppBundle/Controller/MemoController.php
@@ -60,6 +60,83 @@ class MemoController extends Controller
     }
 
     /**
+     * @Route("/{id}/edit", name="memo_edit", requirements={"id"="\d+"})
+     */
+    public function editAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Memo::class);
+        $memo = $repository->find($id);
+
+        if (!$memo) {
+            throw $this->createNotFoundException('メモが見つかりません');
+        }
+
+        return $this->render('memo/edit.html.twig', [
+            'memo' => $memo
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/update", name="memo_update", methods={"POST"}, requirements={"id"="\d+"})
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Memo::class);
+        $memo = $repository->find($id);
+
+        if (!$memo) {
+            throw $this->createNotFoundException('メモが見つかりません');
+        }
+
+        $title = $request->request->get('title');
+        $content = $request->request->get('content');
+
+        if (empty($title) || empty($content)) {
+            $this->addFlash('error', 'タイトルと内容は必須です');
+            return $this->redirectToRoute('memo_edit', ['id' => $id]);
+        }
+
+        $memo->setTitle($title);
+        $memo->setContent($content);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        $this->addFlash('success', 'メモ「' . $title . '」を更新しました！');
+
+        return $this->redirectToRoute('memo_show', ['id' => $id]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="memo_delete", methods={"POST"}, requirements={"id"="\d+"})
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Memo::class);
+        $memo = $repository->find($id);
+
+        if (!$memo) {
+            throw $this->createNotFoundException('メモが見つかりません');
+        }
+
+        $token = $request->request->get('_token');
+        if (!$this->isCsrfTokenValid('delete'.$id, $token)) {
+            $this->addFlash('error', '不正なリクエストです');
+            return $this->redirectToRoute('memo_show', ['id' => $id]);
+        }
+
+        $title = $memo->getTitle();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($memo);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'メモ「' . $title . '」を削除しました');
+
+        return $this->redirectToRoute('memo_index');
+    }
+
+    /**
      * @Route("/{id}", name="memo_show", requirements={"id"="\d+"})
      */
     public function showAction($id)
