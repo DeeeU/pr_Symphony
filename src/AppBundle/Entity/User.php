fbd91bd7a1d5e7c12bd0a8d6f1b144dac7e35716
Muragaruhae <jac8for@gmail.com>
2025-09-05 00:15:24 +0900

    feat: Userエンティティとリポジトリの実装
    - Userエンティティを新規作成
      - id, name, email(unique), password, createdAtフィールドを追加
      - memosプロパティでMemoエンティティとの1:n関係を定義
      - ArrayCollectionを使用してCollection管理
      - コンストラクターでmemos初期化とcreatedAt設定
    
    - UserRepositoryにカスタムメソッドを追加
      - findByEmail(): ログイン機能用のメール検索
      - findByEmailExcluding(): 編集時の重複チェック用
      - createSearchQueryBuilder(): 検索機能付きクエリビルダー
      - findUsersWithMemos(): メモを持つアクティブユーザー取得
      - findRecentUsers(): 最近登録されたユーザー取得
      - getUsersWithMemoCount(): メモ数付きユーザー一覧取得
    
    次の作業: MemoエンティティにauthorフィールドとUser認証機能の実装予定

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var Collection|Memo[]
+     * @ORM\oneToMany(targetEntity="Memo", mappedBy="author")
+     */
+
+    private $memos;
+
+    public function __construct() {
+      $this->memos = new ArrayCollection();
+      $timezone = new \DateTimeZone('Asia/Tokyo');
+      $this->createdAt = new \DateTime('now', $timezone);
+    }
+
+    /**
+     * @return Collection|Memo[]
+     */
+    public function getMemos(): Collection {
+      return $this->memos;
+    }
+
+    public function addMemo(Memo $memo): self {
+      if(!$this->memos->contains($memo)) {
+        $this->memos[] = $memo;
+        $memo->setAuthor($this);
+      }
+      return $this;
+    }
+
+    public function removeMemo(Memo $memo): self {
+      if($this->memos->contains($memo)) {
+        $this->memos->removeElement($memo);
+        if($memo->getAuthor() === $this) {
+          $memo->setAuthor(null);
+        }
+      }
+      return $this;
+    }
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password.
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set createdAt.
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
